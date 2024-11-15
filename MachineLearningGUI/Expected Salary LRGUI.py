162eea420192789fb3e5ba9463a5659f5464eb84
Yassin Mohamed <yassinzx321@gmail.com>
2024-11-15 17:29:27 +0200

    New GUI Uploaded

---
+++ MachineLearningGUI/Expected Salary LRGUI.py
@@ -0,0 +1,57 @@
+from tkinter import*
+import tkinter as tk
+import pandas as pd
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score,mean_squared_error
+import warnings
+warnings.filterwarnings('ignore')
+def LinearRegressions():
+    data = pd.read_csv(r"D:/AMIT/Machine Learning/Linear Regression/Salary_Data.csv") # real
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,1]
+    X_train , X_test , y_train , y_test = train_test_split(X,y,train_size=0.8,random_state = 10)
+    my_model = LinearRegression()
+    my_model.fit(X_train,y_train)
+    y_pred = my_model.predict(X_test)
+    mse = mean_squared_error(y_test,y_pred)
+    rsq = r2_score(y_test,y_pred)
+    print('mean squared error :',mse)
+    print('r square :',rsq)
+    print('Intercept of the model:',my_model.intercept_)
+    print('Coefficient of the line:',my_model.coef_)
+    y_hat = 9357 * 7 +  26089 
+    print(y_hat)
+    x=int(entryRegression.get())
+    y_hat = 9357 * x +  26089 
+    resultRegressionLabel.config(text=f"Expected Salary: {y_hat}")
+    resultRegressionLabel.pack(side="bottom")
+    
+window = Tk()
+window.geometry("1350x700+0+0")
+window.title("Machine Learning")
+window.config(background ="white")
+
+frame = Frame(window,bg="#d9d4d4",highlightbackground="black", highlightthickness=1)
+frame.place(x= 0 , y=75)
+frameButtons = Frame(window)
+frameButtons.place(x= 600 , y=120)
+
+mainTitle = Label(window, text="AMIT Projects", font=("Raleway",40,"bold"), bg="#01468a", fg="white",
+                  highlightbackground='white',highlightthickness=2,pady= 30).place(x= 0, y =0,relwidth=1,relheight=0.05,height = 50)
+regressionLabel = Label(frame, text="Linear Regression" , font = ("Raleway",20,'bold'), bg="#d9d4d4",fg="black").pack(side="top")
+EntryLabel = Label(frameButtons,text="Enter Years: ",font= ('Raleway',10,'bold')).pack(side="left")
+entryRegression = Entry(frameButtons)
+entryRegression.config(font=('Raleway',10),width= 20,bg="#f2f0f0")
+entryRegression.pack()
+entrybutton = Button(frameButtons,text="submit",command=LinearRegressions).pack(side="left")
+resultRegressionLabel = Label(frameButtons,font=('Arial Rounded MT',20),fg="#00ff22")
+label1 = Label(frame, text="Project 1" , font = ("Raleway",20,'bold'), bg="#d9d4d4",fg="black",pady=10).pack(side="top")
+label2 = Label(frame, text="Project 2" , font = ("Raleway",20,'bold'), bg="#d9d4d4",fg="black",pady=10).pack(side="top")
+label3 = Label(frame, text="Project 3" , font = ("Raleway",20,'bold'), bg="#d9d4d4",fg="black",pady=10).pack(side="top")
+label4 = Label(frame, text="Project 4" , font = ("Raleway",20,'bold'), bg="#d9d4d4",fg="black",pady=10).pack(side="top")
+label5 = Label(frame, text="Project 5" , font = ("Raleway",20,'bold'), bg="#d9d4d4",fg="black",pady=10).pack(side="top")
+window.mainloop()
